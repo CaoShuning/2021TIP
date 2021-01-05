729749cd1b7fd660f2d948796b2954f82a12a559
caoshuning <sn_cao@hust.edu.cn>
2021-01-05 10:19:11 +0800

    DCP v1

--- psf_estimation/demo_KernelEstimate.m
+++ psf_estimation/demo_KernelEstimate.m
@@ -8,20 +8,20 @@ Path_G = 'D:\caoshuning\caoshuning\TEST_github\dataset\real\';
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %save path
-SavePath = 'C:\Users\caoshuning\Desktop\Submit_text\result\real\Proposed\h_1001\';
+SavePath = 'C:\Users\caoshuning\Desktop\Submit_text\result\real\Proposed\h_1226\';
 if exist(SavePath,'dir') == 0
     mkdir(SavePath);
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %para setting for real data
-lambda = 5; gamma = 0.5; 
+lambda = 5; gamma = 5; 
 MK =15;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%   
 y_gray = im2double(imread([path, file]));
 [u_dy, h] = destripeDeblur(y_gray,lambda, gamma, MK);
-imwrite(h,[SavePath,'h_', num2str(lambda),'_' num2str(gamma), '_',file]);
+imwrite(h,[SavePath,'h_', num2str(lambda),'_' num2str(gamma), '_',file(1:end-4),'.tif']);
 parsave([SavePath,'h_', num2str(lambda),'_' ,num2str(gamma),'_',file(1:end-4),'.mat'], h, lambda, gamma);    
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function parsave(fname,  h, lambda, gamma)
